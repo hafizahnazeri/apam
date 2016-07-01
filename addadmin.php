324349c942455a42f4310360ca4d9ec81261d68d
hafizahnazeri <izy_911@yahoo.com>
2016-07-01 08:21:46 +0800

    Add files via upload

---
+++ addadmin.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Apam Berry</title>
+<style type="text/css">
+<!--
+body {
+	background-image: url();
+	background-color: #E7CAE8;
+}
+.style1 {font-family: "Times New Roman", Times, serif}
+.style13 {
+	font-family: "Courier New", Courier, monospace;
+	color: #430550;
+	font-weight: bold;
+	font-size: x-large;
+}
+.style19 {font-family: "Courier New", Courier, monospace; color: #430550; font-weight: bold; font-size: large; }
+.style23 {color: #430550; font-size: x-large; }
+.style24 {color: #430550; font-size: large; font-family: "Courier New", Courier, monospace;}
+-->
+</style>
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+</head>
+
+
+<body>
+
+
+<table width="100%" align="center"><tr><td height="232" bordercolor="#F807CE"><div align="center">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <table width="100%">
+    <tr>
+      <td height="232" bordercolor="#EC008C"><div align="center">
+          <table width="180">
+            <tr>
+              <td width="170"><a href="adminlogin.html"><img src="picture/logo baru.png" alt="" width="170" height="121" /></a></td>
+            </tr>
+          </table>
+          <table width="496" height="265" align="center">
+            <tr>
+              <td width="520" height="87" bgcolor="#EC008C"><form  method="post" action="addadmin2.php">
+                  <label></label>
+                  <table width="488" height="256" align="center" cellspacing="5">
+                    <tr>
+                      <td width="506" height="246" bgcolor="#E7CAE8"><div align="left">
+                          <div align="center">
+                            <p><span class="style13"> Sign Up Admin</span></p>
+                          </div>
+                        <table width="476" align="center">
+                            <tr class="style24">
+                              <td width="185"><span class="style24">Name</span> </td>
+                              <td width="11" class="style24"><span class="style24">:</span></td>
+                              <td width="264" class="style24"><label>
+                                <input name="name" type="text" class="style19" id="name" />
+                              </label></td>
+                            </tr>
+                            <tr class="style24">
+                              <td><span class="style24">Username</span> </td>
+                              <td class="style24"><span class="style24">:</span></td>
+                              <td class="style24"><label>
+                                <input name="username" type="text" class="style19" id="username" />
+                              </label></td>
+                            </tr>
+                            <tr class="style24">
+                              <td><span class="style24">Password</span> </td>
+                              <td class="style24"><span class="style24">:</span></td>
+                              <td class="style24"><label>
+                                <input name="password" type="password" class="style19" id="password3" />
+                              </label></td>
+                            </tr>
+                            
+                            <tr class="style24">
+                              <td>Re-type password</td>
+                              <td class="style24">:</td>
+                              <td class="style24"><input name="password2" type="password" class="style19" id="password" /></td>
+                            </tr>
+                          </table>
+                        <p align="center">
+                            <label></label>
+                            <label>
+                            <input name="button" type="submit" class="style19" id="button" value="Submit" />
+                            </label>
+                            <label>
+                            <input name="button2" type="reset" class="style19" id="button2" value="Reset" />
+                            </label></p>
+                        </td>
+                    </tr>
+                  </table>
+              </form></td>
+            </tr>
+          </table>
+          
+          
+         
+          <p>&nbsp;</p>
+      </div>
+          <div align="center" class="style1">
+            <div align="center">All Rights Reserved. © 2013 Apam Berry. </div>
+          </div></td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+</div>
+      </td>
+  </tr>
+</table>
+
+</body>
+</html>
